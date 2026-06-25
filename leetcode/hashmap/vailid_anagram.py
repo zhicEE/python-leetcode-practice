aa09549641cc1f163524df679fa620a06665497c
Zhi Chen <zhic33@gmail.com>
2026-06-25 16:48:43 +0800

    solve LeetCode 242: Valid Anagram using hash map

---
+++ leetcode/hashmap/vailid_anagram.py
@@ -0,0 +1,43 @@
+# manual test
+s = "anagram"
+t = "nagaram"
+
+count = {}
+
+for ch in s:
+    count[ch] = count.get(ch, 0) + 1
+
+for ch in t:
+    count[ch] = count.get(ch, 0) - 1
+
+for value in count.values():
+    if value != 0:
+        print(False)
+        break
+else:
+    print(True)
+
+
+# function solution
+def isAnagram(s, t):
+
+    if len(s) != len(t):
+        return False
+
+    count = {}
+
+    for ch in s:
+        count[ch] = count.get(ch, 0) + 1 
+
+    for ch in t:
+        count[ch] = count.get(ch, 0) - 1
+
+    for v in count.values():
+        if v != 0:
+            return False
+        
+    return True
+
+# test
+print(isAnagram("anagram", "nagaram"))
+print(isAnagram("rat", "car"))
