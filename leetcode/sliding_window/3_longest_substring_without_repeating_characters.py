887e87ea25cd67399de2bfaaaa0ede36da636ac9
Zhi Chen <zhic33@gmail.com>
2026-08-17 23:43:31 +0800

    review LeetCode 3: Longest Substring Without Repeating Characters

--- leetcode/sliding_window/3_longest_substring_without_repeating_characters.py
+++ leetcode/sliding_window/3_longest_substring_without_repeating_characters.py
@@ -58,3 +58,23 @@ def lengthOfLongestSubstring(self, s: str) -> int:
         max_length = max(max_length, right - left + 1)
 
     return max_length
+
+# 2026-08-17 Review
+def lengthOfLongestSubstring(self, s: str) -> int:
+
+    window = set()
+    left = 0
+    max_length = 0
+
+    for right in range(len(s)):
+
+        while s[right] in window:
+
+            window.remove(s[left])
+            left += 1
+
+        window.add(s[right])
+
+        max_length = max(max_length, right - left + 1)
+
+    return max_length
